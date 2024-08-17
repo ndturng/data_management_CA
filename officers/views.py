bef0e2ac8ef764e6679f611d74fe343f3a2cf039
Nguyen Duong Trung <103947618+ndturng@users.noreply.github.com>
2024-08-17 08:49:56 +0000

    fix date import

--- officers/views.py
+++ officers/views.py
@@ -1,15 +1,31 @@
 import pandas as pd
 from django.shortcuts import get_object_or_404, redirect, render
-
+from datetime import datetime
 from .forms import ExcelUploadForm, OfficerInfoForm
 from .models import Officer
 
 
 def get_day(row, column):
-    day = row.get(column, None)
-    date_time = pd.to_datetime(day, errors="coerce").date()
-    return date_time if day else None
-
+    try:
+        day = row.get(column, None)
+        # print(f"Raw value for day: {day}, Type: {type(day)}")
+        
+        if isinstance(day, str):
+            # Handle string dates
+            date_time = datetime.strptime(day, "%d/%m/%Y")
+        elif isinstance(day, pd.Timestamp):
+            # If it's a pandas Timestamp, we can convert it directly
+            date_time = day.to_pydatetime()
+        elif isinstance(day, datetime):
+            # If it's already a datetime object, just return it
+            date_time = day
+        else:
+            return None
+        
+        return date_time
+    except ValueError:
+        print(f"Failed to parse date for {row['name']} with value: {day}")
+        return None
 
 def officer_list(request):
     officers = Officer.objects.all()
@@ -42,35 +58,35 @@ def excel_upload(request):
             # Iterate over the rows and create Officer objects
             for _, row in data.iterrows():
                 Officer.objects.create(
-                    name=row.get("name", ""),
-                    date_of_birth=get_day(row, "date_of_birth"),
-                    current_residence=row.get("current_residence", ""),
-                    id_ca=row.get("id_ca", ""),
-                    id_citizen=row.get("id_citizen", ""),
-                    gender=row.get("gender", ""),
-                    date_of_enlistment=get_day(row, "date_of_enlistment"),
-                    date_join_party=get_day(row, "date_join_party"),
-                    home_town=row.get("home_town", ""),
-                    blood_type=row.get("blood_type", ""),
-                    education=row.get("education", ""),
-                    certi_of_IT=row.get("certi_of_IT", ""),
+                    name=row.get("Họ và tên", ""),
+                    date_of_birth=get_day(row, "Ngày,tháng, năm sinh"),
+                    current_residence=row.get("Chổ ở hiện nay: Thôn ( Khu Phố)", ""), # noqa
+                    id_ca=row.get("Số hiệu", ""),
+                    id_citizen=row.get("Số CMND", ""),
+                    gender=row.get("Giới tính", ""),
+                    date_of_enlistment=get_day(row, "Vào ngành"),
+                    date_join_party=get_day(row, "Vào đảng"),
+                    home_town=row.get("Quê quán", ""),
+                    blood_type=row.get("Nhóm Máu", ""),
+                    education=row.get("Trình độ", ""),
+                    certi_of_IT=row.get("Tin học", ""),
                     certi_of_foreign_language=row.get(
-                        "certi_of_foreign_language", ""
+                        "Ngoại Ngữ", ""
                     ),
-                    political_theory=row.get("political_theory", ""),
-                    military_rank=row.get("military_rank", ""),
-                    rank_type=row.get("rank_type", ""),
-                    position=row.get("position", ""),
-                    work_unit=row.get("work_unit", ""),
-                    military_type=row.get("military_type", ""),
-                    equipment_type=row.get("equipment_type", ""),
-                    size_of_shoes=row.get("size_of_shoes", ""),
-                    size_of_hat=row.get("size_of_hat", ""),
-                    size_of_clothes=row.get("size_of_clothes", ""),
-                    bank_account_BIDV=row.get("bank_account_BIDV", ""),
-                    phone_number=row.get("phone_number", ""),
-                    laudatory=row.get("laudatory", ""),
-                    punishment=row.get("punishment", ""),
+                    political_theory=row.get("Trình độ LLCT", ""),
+                    military_rank=row.get("Cấp bậc hàm", ""),
+                    rank_type=row.get("Loại hàm", ""),
+                    position=row.get("Chức vụ", ""),
+                    work_unit=row.get("Vị trí công tác", ""),  # noqa # Đơn vị công tác
+                    military_type=row.get("Lực lượng", ""),
+                    equipment_type=row.get("Quân trang", ""),
+                    size_of_shoes=row.get("Giày", ""),
+                    size_of_hat=row.get("Mũ", ""),
+                    size_of_clothes=row.get("Quần áo", ""),
+                    bank_account_BIDV=row.get("Số tài khoản BIDV", ""),
+                    phone_number=row.get("Số Điện thoại", ""),
+                    laudatory=row.get("Khen thưởng", ""),
+                    punishment=row.get("Kỷ luật", ""),
                 )
             return redirect(
                 "officer_list"
