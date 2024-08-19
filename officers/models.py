1cddda39a0e9722df39f877487d721708ba9bc92
Nguyen Duong Trung <103947618+ndturng@users.noreply.github.com>
2024-08-19 02:02:14 +0000

    chanel state of data field

--- officers/models.py
+++ officers/models.py
@@ -2,39 +2,39 @@ from django.db import models
 
 
 class Officer(models.Model):
-    id_ca = models.CharField(max_length=255, default="unknown")
-    id_citizen = models.CharField(max_length=255, default="unknown")
-    name = models.CharField(max_length=255, default="unknown")
-    gender = models.CharField(max_length=255, default="unknown")
+    name = models.CharField(max_length=255, null=True, blank=False)
+    id_ca = models.CharField(max_length=255, null=True, blank=True)
+    id_citizen = models.CharField(max_length=255, null=True, blank=True)
+    gender = models.CharField(max_length=255, null=True, blank=False)
     date_of_birth = models.DateField(
-        null=True
+        null=True, blank=True
     )  # handle the case just the month and year
-    date_of_enlistment = models.DateField(null=True)
-    date_join_party = models.DateField(null=True)
-    home_town = models.CharField(max_length=255, default="unknown")
+    date_of_enlistment = models.DateField(null=True, blank=True)
+    date_join_party = models.DateField(null=True, blank=True)
+    home_town = models.CharField(max_length=255, null=True, blank=False)
     current_residence = models.CharField(
-        max_length=255, default="unknown"
+        max_length=255, null=True, blank=False
     )  # address
-    blood_type = models.CharField(max_length=255, default="unknown")
-    education = models.CharField(max_length=255, default="unknown")
-    certi_of_IT = models.CharField(max_length=255, default="unknown")
+    blood_type = models.CharField(max_length=255, null=True, blank=True)
+    education = models.CharField(max_length=255, null=True, blank=True)
+    certi_of_IT = models.CharField(max_length=255, null=True, blank=True)
     certi_of_foreign_language = models.CharField(
-        max_length=255, default="unknown"
+        max_length=255, null=True, blank=True
     )
-    political_theory = models.CharField(max_length=255, default="unknown")
-    military_rank = models.CharField(max_length=255, default="unknown")
-    rank_type = models.CharField(max_length=255, default="unknown")
-    position = models.CharField(max_length=255, default="unknown")
-    work_unit = models.CharField(max_length=255, default="unknown")
-    military_type = models.CharField(max_length=255, default="unknown")
-    equipment_type = models.CharField(max_length=255, default="unknown")
-    size_of_shoes = models.CharField(max_length=255, default="unknown")
-    size_of_hat = models.CharField(max_length=255, default="unknown")
-    size_of_clothes = models.CharField(max_length=255, default="unknown")
-    bank_account_BIDV = models.CharField(max_length=255, default="unknown")
-    phone_number = models.CharField(max_length=255, default="unknown")
-    laudatory = models.CharField(max_length=255, default="unknown")
-    punishment = models.CharField(max_length=255, default="unknown")
+    political_theory = models.CharField(max_length=255, null=True, blank=True)
+    military_rank = models.CharField(max_length=255, null=True, blank=True)
+    rank_type = models.CharField(max_length=255, null=True, blank=True)
+    position = models.CharField(max_length=255, null=True, blank=True)
+    work_unit = models.CharField(max_length=255, null=True, blank=True)
+    military_type = models.CharField(max_length=255, null=True, blank=True)
+    equipment_type = models.CharField(max_length=255, null=True, blank=True)
+    size_of_shoes = models.CharField(max_length=255, null=True, blank=True)
+    size_of_hat = models.CharField(max_length=255, null=True, blank=True)
+    size_of_clothes = models.CharField(max_length=255, null=True, blank=True)
+    bank_account_BIDV = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=255, null=True, blank=True)
+    laudatory = models.CharField(max_length=255, null=True, blank=True)
+    punishment = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
